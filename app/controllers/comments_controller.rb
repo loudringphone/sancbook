8ad65acd292d0237158d27898125066541ce9c18
Wing Fung Lau <wingfunglau@Wings-MacBook-Pro.local>
2023-01-24 15:25:38 +1100

    making heroku great again

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,8 @@
 class CommentsController < ApplicationController
     before_action :check_for_login
     def create
+        @comment = params[:comment]["text"]
         raise 's'
+        redirect_to '/sanctions/15'
     end
 end
