fc8aa820a3e94f7bd17e57c9f949c6e15123654f
Wing Fung Lau <wingfunglau@Wings-MacBook-Pro.local>
2023-01-24 16:22:23 +1100

    making heroku great again

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,14 @@ class CommentsController < ApplicationController
         comment = Comment.new
         comment.text = params[:comment]["text"]
         comment.user_id = @current_user.id
+
+        location = request.headers["HTTP_REFERER"]
+        uri    = URI.parse(location)
+        params = CGI.parse(uri.query)
+
+
+
+
         comment.sanction_id = params[:id]
         comment.sent = Time.now.strftime("%d/%m/%Y %H:%M")
         raise 's'
