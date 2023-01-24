5eab509695208381f3ddd9047810c5cf5d7fab61
Wing Fung Lau <wingfunglau@Wings-MacBook-Pro.local>
2023-01-24 16:15:55 +1100

    making heroku great again

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,17 @@
 class CommentsController < ApplicationController
     before_action :check_for_login
     def create
-        @comment = params[:comment]["text"]
+        comment = Comment.new
+        comment.text = params[:comment]["text"]
+        comment.user_id = @current_user.id
+        comment.sanction_id = params[:id]
+        comment.sent = Time.now.strftime("%d/%m/%Y %H:%M")
+        raise 's'
+        comment.save
+
+        @comment
         location = request.headers["HTTP_REFERER"]
         redirect_to location
     end
+
 end
